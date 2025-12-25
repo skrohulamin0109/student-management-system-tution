29e6950c8246dc88a15de276aa8719c3dd38f032
Sk Rohul Amin <skrohulamin0109@gmail.com>
2025-12-25 22:23:24 +0530

    change nav from flex box to grid and add company_logo_container

--- project-directory/student-management-system-tution/src/assets/components/NavBar/NavBarOptions.jsx
+++ project-directory/student-management-system-tution/src/assets/components/NavBar/NavBarOptions.jsx
@@ -4,6 +4,10 @@ const NavBarOptions = () => {
   let navOptions = [ 'Teachers Section', 'Students Section', 'Fees & Dates', 'Contact Me']
   return (
     <>
+    <div className={styles.company_logo_container}>
+
+    </div>
+
     <nav className={styles.nav}>
       <ul className={styles.ul_container}>
        {navOptions.map((item, index) =>(
