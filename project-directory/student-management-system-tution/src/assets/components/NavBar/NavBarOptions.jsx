967a8781e57f67cb244ee8b80a430bde145dc015
Sk Rohul Amin <skrohulamin0109@gmail.com>
2025-12-25 22:44:31 +0530

    add company_logo inside company_logo_container

--- project-directory/student-management-system-tution/src/assets/components/NavBar/NavBarOptions.jsx
+++ project-directory/student-management-system-tution/src/assets/components/NavBar/NavBarOptions.jsx
@@ -1,14 +1,17 @@
 import styles from "./NavBarOptions.module.css";
+import companyLogo from "../../images/company_logo.png";
+
+
 
 const NavBarOptions = () => {
   let navOptions = [ 'Teachers Section', 'Students Section', 'Fees & Dates', 'Contact Me']
   return (
     <>
-    <div className={styles.company_logo_container}>
-      <img src="" alt="company logo" className={styles.company_logo} />
-    </div>
 
     <nav className={styles.nav}>
+    <div className={styles.company_logo_container}>
+      <img src={companyLogo} alt="company logo" className={styles.company_logo} />
+    </div>
       <ul className={styles.ul_container}>
        {navOptions.map((item, index) =>(
         <li key={index} className={styles.li_items}>{item}</li>)
