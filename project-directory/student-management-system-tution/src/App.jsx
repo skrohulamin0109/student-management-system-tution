476bf819e810362b4d6a6427675479237f14259a
Sk Rohul Amin <skrohulamin0109@gmail.com>
2026-01-09 21:11:43 +0530

    delete all components and styles for fresh start

--- project-directory/student-management-system-tution/src/App.jsx
+++ project-directory/student-management-system-tution/src/App.jsx
@@ -1,21 +1,6 @@
-import NavBarOptions from "./assets/components/NavBar/NavBarOptions.jsx"
-import styles from "./App.module.css"
-import BatchesSection from "./assets/components/HeroSection/BatchesSection/BatchesSection.jsx"
+import styles from "./App.module.css";
 function App() {
- 
-
-  return (
-    <>
-    <div className={styles.mainContainer}>
-    <NavBarOptions></NavBarOptions>
-    
-    <div className={styles.contentContainer}>
-      <BatchesSection></BatchesSection>
-    </div>
-
-    </div>
-    </>
-  )
+    return <></>;
 }
 
-export default App
+export default App;
